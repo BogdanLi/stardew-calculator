d5c2246a808169bb83cb4c6b5b814bd7e31b26b4
BogdanLi <li.bogdan44@gmail.com>
2025-01-22 11:53:43 +0500

    fonts and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 export default function App() {
   return (
-    <div>
-      <h1>Stardew Valley Calculator</h1>
-    </div>
+    <main className="h-dvh md:h-screen grid place-items-center">
+      <div className="bg-white">
+        <h1 className="text-4xl">Stardew Calculator</h1>
+      </div>
+    </main>
   );
 }
